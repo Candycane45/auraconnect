914e0ba8c35c1c472cd0fe0275b40f6901c39549
User287 <111180856+klee3@users.noreply.github.com>
2025-08-16 21:18:01 +0530

    updated the api url path

--- frontend/src/lib/constant.ts
+++ frontend/src/lib/constant.ts
@@ -4,5 +4,4 @@ export const FIREBASE_AUTH_DOMAIN =
   process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
 export const FIREBASE_PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 
-// export const API_URL = "http://localhost:5000";
-export const API_URL = "https://buck-falls-cet-fatty.trycloudflare.com";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL!;
